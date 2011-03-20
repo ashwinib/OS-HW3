21a97490d5e437bee6b84059db5e9ba9300f9b83
ashwinib <ashwini.p.bhat@gmail.com>
2011-03-20 13:40:12 -0400

    Working:gets counts except for 'a' todo:make task manager,add delims

--- p5/p5.cu
+++ p5/p5.cu
@@ -6,7 +6,7 @@
 #define MAX_HASH_ENTRIES 200
 #define M 10
 
-__global__ void getWordCounts(char *fileArray,int *countArray,int *fileSize,char *wordhashtable, int *nextPtr){
+__global__ void getWordCounts(char *fileArray,int *countArray,int *fileSize,char *wordhashtable, int *nextPtr, int *lock){
   unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
   int ind,word_started =0 ,count =0;
   int found;
@@ -16,31 +16,40 @@ __global__ void getWordCounts(char *fileArray,int *countArray,int *fileSize,char
   int  tempi=0,tempi2;
 
   for(ind =0;ind<fileSize[i];ind++){
+    if(ptr[ind]>64&&ptr[ind]<91) ptr[ind]+=32;
     if(ptr[ind]!=' '&&ptr[ind]!='.'&&ptr[ind]!='!')
       if(word_started!=1) {
 	word_started = 1;
-	hashvalue = (ptr[ind]>64&&ptr[ind]<91) ? ptr[ind]+32:ptr[ind];//temp addition else do only assignemnt
+	hashvalue = ptr[ind];//>64&&ptr[ind]<91) ? ptr[ind]+32:ptr[ind];//temp addition else do only assignemnt
 	wptr = &ptr[ind];
       }
       else{//Middle of the word
-	hashvalue+= (ptr[ind]>64&&ptr[ind]<91) ? ptr[ind]+32:ptr[ind];
+	hashvalue+= ptr[ind];//>64&&ptr[ind]<91) ? ptr[ind]+32:ptr[ind];
       }
     if(word_started)
       if(ptr[ind]==' '||ptr[ind]=='.'||ptr[ind]=='!'){
         word_started = 0;
 	hashvalue = hashvalue % M;// 10 here is hashtable size M
-	
+
+	/*Check Location*/	
+	//lock -hashvalue
+	while(!atomicCAS(&lock[hashvalue],0,1));
 	if(wordhashtable[hashvalue*20]=='\0'){//Not found in Hash
 	  temp = &wordhashtable[hashvalue*20];
 	  tempi =0;
-	  while(&wptr[tempi]!=&ptr[ind])
+	  while(&wptr[tempi]!=&ptr[ind])//Entering in hash table
 		{temp[tempi]= wptr[tempi];
 		tempi++;}
-	  atomicAdd(&countArray[hashvalue],1);//count
+	//unlock -hash value
+	  atomicCAS(&lock[hashvalue],1,0);
+	  //fn-atomicAdd(&countArray[hashvalue],1);//count
+
+  countArray[hashvalue] = hashvalue; 
 	}
 	else{//Collision detection
 	  tempi =hashvalue;found = -1;
-
+	
+	 /*Check word*/
 	  while(nextPtr[tempi]!=-1||found==-1){
 	    tempi2 = 0;
 	    found =1;
@@ -49,26 +58,39 @@ __global__ void getWordCounts(char *fileArray,int *countArray,int *fileSize,char
 	      if(temp[tempi2]!=wptr[tempi2]) {found =0;break;}
 	      tempi2++;
 	    }
+	    if(temp[tempi2]!='\0') found =0;
+	    //unlock - tempi
+	    atomicCAS(&lock[tempi],1,0);
 	    if(found) break;
-	    if(nextPtr[tempi]!=-1)
+	    if(nextPtr[tempi]!=-1){
 	       	tempi = nextPtr[tempi];      
+		//lock - tempi
+		while(!atomicCAS(&lock[tempi],0,1));
+	    }
 	  }
 
-	  if(found)
+	  if(found){
 	    atomicAdd(&countArray[tempi],1);
-		//countArray[tempi]=hashvalue;}//DEBUG
+		countArray[tempi]=hashvalue;}//DEBUG
 	  else{//Collision but record not found
 	    tempi2 =0;
+	    //lock - M+tempi2
+	    while(!atomicCAS(&lock[M+tempi2],0,1));
 	    while(wordhashtable[(M+tempi2)*20]!='\0' && tempi2<MAX_HASH_ENTRIES) tempi2++;//10 = M; tempi2 holds location in hast tab;e
 	    if(tempi2 < MAX_HASH_ENTRIES){
 	    	nextPtr[tempi] = tempi2+M;tempi=0;//tempi holds the location where last hash was found
 	        temp = &wordhashtable[(M+tempi2)*20];
-		while(&wptr[tempi]!=&ptr[ind])
-			{temp[tempi]=wptr[tempi]; 
+		while(&wptr[tempi]!=&ptr[ind]) //Entering in hash table
+			{temp[tempi]= wptr[tempi]; 
 			tempi++;}
-		atomicAdd(&countArray[tempi2+M],1);
-		//countArray[tempi2+10]=hashvalue;//DEBUG
+		//unlock - M+tempi2
+	        atomicCAS(&lock[M+tempi2],1,0);
+
+  countArray[tempi2+M] = hashvalue; 
+		//fn-atomicAdd(&countArray[tempi2+M],1);
 	    }//count*/
+	    //tryunlock = M+tempi2
+	    atomicCAS(&lock[M+tempi2],1,0);
 	  }
 
 	}
@@ -92,6 +114,7 @@ int main(int argc,char **argv){
   char *dhashtable; 
   int *nextPtr;
   int *dnextPtr;
+  int *dlock;
   int noOfFiles=0;
   FILE *fp;
 
@@ -108,10 +131,12 @@ int main(int argc,char **argv){
   cudaMalloc((void**)&dfileSize,10*sizeof(int));
   cudaMalloc((void**)&dhashtable,20*MAX_HASH_ENTRIES*sizeof(char));//20-max word size 500-max words
   cudaMalloc((void**)&dnextPtr,MAX_HASH_ENTRIES*sizeof(int));//corresponding counts of words
+  cudaMalloc((void**)&dlock,MAX_HASH_ENTRIES*sizeof(int));//corresponding counts of words
 
   cudaMemset(dcountArray,0,MAX_HASH_ENTRIES*sizeof(int));
   cudaMemset(dhashtable,'\0',20*MAX_HASH_ENTRIES*sizeof(char));
   cudaMemset(dnextPtr,-1,MAX_HASH_ENTRIES*sizeof(int));
+  cudaMemset(dlock,0,MAX_HASH_ENTRIES*sizeof(int));
   
   while(scanf("%s",filename)!=EOF){
     printf("\nAttempting to open %s",filename);
@@ -133,7 +158,7 @@ int main(int argc,char **argv){
   }
   cudaMemcpy(dfileArray,fileArray,10*MAX_FILE_SIZE*sizeof(char),cudaMemcpyHostToDevice);
   cudaMemcpy(dfileSize,fileSize,10*sizeof(int),cudaMemcpyHostToDevice);
-  getWordCounts<<<1,noOfFiles>>>(dfileArray,dcountArray,dfileSize,dhashtable,dnextPtr);
+  getWordCounts<<<1,noOfFiles>>>(dfileArray,dcountArray,dfileSize,dhashtable,dnextPtr, dlock);
   cudaThreadSynchronize();
   cudaMemcpy(countArray,dcountArray,200*sizeof(int),cudaMemcpyDeviceToHost);
   cudaMemcpy(hashtable,dhashtable,20*200*sizeof(char),cudaMemcpyDeviceToHost);
